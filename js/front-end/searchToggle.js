f6db3ab89d6a725ddb8d188821d9414ec3dc352d
Eric Boyer <eboyer@bluemodus.com>
2017-04-05 11:20:49 -0600

    polish header animations

--- js/front-end/searchToggle.js
+++ js/front-end/searchToggle.js
@@ -1,28 +1,34 @@
 //toggle search on lg screens
 
 jQuery(document).ready(function($) {
-    var mqlSrch = window.matchMedia("(min-width: 768px)");
+  var mqlSrch = window.matchMedia("(min-width: 768px)");
 
-    function toggleSearch() {
-        $('.headerMain-searchToggler').on("click.mqlSrch", function() {
-            $('.headerMain').toggleClass("search-open");
-        }); 
-    }
-
-    function resetIt() {
-        $('.headerMain-searchToggler').off("click.mqlSrch");
-        $('.headerMain').removeClass('search-open');
-    }
-
-    function screenSize(mqlSrch) {
-      if (mqlSrch.matches) {
-        toggleSearch();
+  function toggleSearch() {
+    $('.headerMain-searchToggler').on("click.mqlSrch", function() {
+      if (!$('.headerMain').hasClass('search-open')) {
+        $('.headerMain').addClass('search-open');
+        $('.headerMain-searchBox').focus();
       } else {
-        resetIt();
+        $('.headerMain').removeClass('search-open');
+        $('.headerMain-searchBox').blur();
       }
-    }
+    });
+  }
 
-    // Handle media query 'change' event
-    mqlSrch.addListener(screenSize);
-    screenSize(mqlSrch);
+  function resetIt() {
+    $('.headerMain-searchToggler').off("click.mqlSrch");
+    $('.headerMain').removeClass('search-open');
+  }
+
+  function screenSize(mqlSrch) {
+    if (mqlSrch.matches) {
+      toggleSearch();
+    } else {
+      resetIt();
+    }
+  }
+
+  // Handle media query 'change' event
+  mqlSrch.addListener(screenSize);
+  screenSize(mqlSrch);
 });
